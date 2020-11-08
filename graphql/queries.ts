0682ed5bda565f112b5cadf92ff6673e982471ff
Edouardo Vitale <35499973+eddyvi@users.noreply.github.com>
2020-11-08 15:39:08 -0500

    add cam and equipment to api

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -40,3 +40,61 @@ export const listTodos = /* GraphQL */ `
     }
   }
 `;
+export const getCamera = /* GraphQL */ `
+  query GetCamera($id: ID!) {
+    getCamera(id: $id) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listCameras = /* GraphQL */ `
+  query ListCameras(
+    $filter: ModelCameraFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listCameras(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getEquipment = /* GraphQL */ `
+  query GetEquipment($id: ID!) {
+    getEquipment(id: $id) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listEquipments = /* GraphQL */ `
+  query ListEquipments(
+    $filter: ModelEquipmentFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listEquipments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
--- graphql/subscriptions.ts
+++ graphql/subscriptions.ts
@@ -44,3 +44,69 @@ export const onDeleteTodo = /* GraphQL */ `
     }
   }
 `;
+export const onCreateCamera = /* GraphQL */ `
+  subscription OnCreateCamera {
+    onCreateCamera {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateCamera = /* GraphQL */ `
+  subscription OnUpdateCamera {
+    onUpdateCamera {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteCamera = /* GraphQL */ `
+  subscription OnDeleteCamera {
+    onDeleteCamera {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateEquipment = /* GraphQL */ `
+  subscription OnCreateEquipment {
+    onCreateEquipment {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateEquipment = /* GraphQL */ `
+  subscription OnUpdateEquipment {
+    onUpdateEquipment {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteEquipment = /* GraphQL */ `
+  subscription OnDeleteEquipment {
+    onDeleteEquipment {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
